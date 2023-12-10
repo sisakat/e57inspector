861dc4a70d9ffb38df73d165d917117a5d239b63
Stefan Isak <isakhandy@gmail.com>
2023-12-10 10:42:43 +0100

    Refactoring

--- app/SceneView.h
+++ app/SceneView.h
@@ -1,26 +1,26 @@
 #ifndef SIPOINTCLOUDRENDERER_H
 #define SIPOINTCLOUDRENDERER_H
 
-#include <QWidget>
+#include <QMatrix4x4>
+#include <QOpenGLDebugLogger>
 #include <QOpenGLFunctions>
 #include <QOpenGLFunctions_3_3_Core>
-#include <QOpenGLDebugLogger>
 #include <QOpenGLWidget>
-#include <QMatrix4x4>
-#include <vector>
+#include <QWidget>
 #include <memory>
+#include <vector>
 
-#include "octree.h"
-#include "scene.h"
 #include "camera.h"
+#include "octree.h"
 #include "pointcloud.h"
+#include "scene.h"
 #include "shader.h"
 
 class SceneView : public QOpenGLWidget, protected QOpenGLFunctions_3_3_Core
 {
     Q_OBJECT
 public:
-    explicit SceneView(QWidget *parent = nullptr);
+    explicit SceneView(QWidget* parent = nullptr);
     ~SceneView() override;
 
     Scene& scene();
@@ -29,22 +29,18 @@ protected:
     void initializeGL() override;
     void paintGL() override;
     void resizeGL(int width, int height) override;
-    void mousePressEvent(QMouseEvent *event) override;
-    void mouseReleaseEvent(QMouseEvent *event) override;
-    void mouseMoveEvent(QMouseEvent *event) override;
-    void wheelEvent(QWheelEvent *event) override;
-    void keyPressEvent(QKeyEvent *event) override;
-    void keyReleaseEvent(QKeyEvent *event) override;
+    void mousePressEvent(QMouseEvent* event) override;
+    void mouseReleaseEvent(QMouseEvent* event) override;
+    void mouseMoveEvent(QMouseEvent* event) override;
+    void wheelEvent(QWheelEvent* event) override;
+    void keyPressEvent(QKeyEvent* event) override;
+    void keyReleaseEvent(QKeyEvent* event) override;
 
 private slots:
-    void onMessageLogged(const QOpenGLDebugMessage &debugMessage);
+    void onMessageLogged(const QOpenGLDebugMessage& debugMessage);
 
 private:
     QOpenGLDebugLogger m_openGLLogger;
-
-    QMatrix4x4 m_model;
-    QMatrix4x4 m_view;
-    QMatrix4x4 m_projection;
 
     Scene::Ptr m_scene;
     Shader::Ptr m_shader;
