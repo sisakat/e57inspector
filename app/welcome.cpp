29d84b4c515efbc36eb50078701e3908e5604621
Stefan Isak <isakhandy@gmail.com>
2023-12-29 22:48:30 +0100

    Rework welcome screen

--- app/welcome.cpp
+++ app/welcome.cpp
@@ -1,9 +1,20 @@
 #include "welcome.h"
 #include "./ui_welcome.h"
+#include <QFile>
 
 Welcome::Welcome(QWidget* parent) : QWidget(parent), ui(new Ui::Welcome)
 {
     ui->setupUi(this);
+
+    QFile welcomeFile(":/text/welcome.html");
+    if (welcomeFile.open(QIODevice::ReadOnly))
+    {
+        auto* document = new QTextDocument(ui->textBrowser);
+        document->setHtml(welcomeFile.readAll());
+        document->setDocumentMargin(10);
+        ui->textBrowser->setDocument(document);
+        ui->textBrowser->setReadOnly(true);
+    }
 }
 
 Welcome::~Welcome() = default;
