0b5a19790b94cedaf67d1c15321fdcee43d96c9a
Stefan Isak <isakhandy@gmail.com>
2026-02-19 11:08:32 +0100

    fix rendering issues on macOS #6

--- app/main.cpp
+++ app/main.cpp
@@ -4,6 +4,11 @@
 
 int main(int argc, char* argv[])
 {
+    QSurfaceFormat format;
+    format.setVersion(3, 3);
+    format.setProfile(QSurfaceFormat::CoreProfile);
+    QSurfaceFormat::setDefaultFormat(format);
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
