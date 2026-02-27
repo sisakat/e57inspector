84e8401f4c6e9960b4d475a7ee0722ab3390a00e
Stefan Isak <isakhandy@gmail.com>
2026-02-27 08:24:38 +0100

    install libs for gui

--- ci/build_ubuntu.sh
+++ ci/build_ubuntu.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y build-essential qt6-base-dev qt6-tools-dev libxerces-c-dev
+sudo apt install -y \
+  build-essential \
+  qt6-base-dev \
+  qt6-base-dev-tools \
+  qt6-tools-dev \
+  libgl1-mesa-dev \
+  libglu1-mesa-dev \
+  libx11-dev \
+  libxext-dev \
+  libxrender-dev \
+  libxcb1-dev \
+  libxkbcommon-x11-dev \
+  libxi-dev \
+  libxerces-c-dev
 cmake -B build -DCMAKE_BUILD_TYPE=$BUILD_TYPE
 cmake --build build --config $BUILD_TYPE
